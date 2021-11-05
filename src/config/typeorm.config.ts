c4cec0c2f419ce31061e2b6e788f0bcfba61ac4f
AmmarYousaf6 <ammaryousaf6@gmail.com>
2021-11-05 20:54:34 +0500

    pushing changes for authentication

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,11 +2,11 @@ import {TypeOrmModuleOptions} from "@nestjs/typeorm";
 
 export const typeOrmConfig : TypeOrmModuleOptions = {
     type : 'postgres',
-    host : 'localhost',
+    host : 'ec2-34-242-89-204.eu-west-1.compute.amazonaws.com',
     port : 5432,
-    username : 'tecmint',
-    password : 'Ammar123',
-    database : 'taskmanagement',
+    username : 'lgrzgulpsrgoxy',
+    password : '0111cdee2e0c1c96b8f757a18985d727b08751b5e78fb1006e2f23c704172975',
+    database : 'd7dq5qmihjekr2',
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize : true
 }
